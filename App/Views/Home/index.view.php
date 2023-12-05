dd93273bfe9c62da4fb10f49806f0679e6e96b63
Andrej Brnusak <adobrnusak@gmail.com>
2023-12-05 18:45:19 +0100

    home page view change root.layout.view.php

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -1,31 +1,76 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col mt-5">
-            <div class="text-center">
-                <h2>Vaííčko MVC FW</h2>
-                <img src="public/images/vaiicko_logo.png">
-                <p>
-                    Gratulujeme vám, úspešne sa vám podarilo nainštalovať a spustiť framework
-                    <strong>Vaííčko</strong> <?= \App\Config\Configuration::FW_VERSION ?>!<br>
-                    Dúfame, že sa Vám pomocou tohto frameworku vytvoríte nejakú skvelú aplikáciu.<br>
-                </p>
-                <p>
-                    Tento jednoduchý framework bol vytvorený pre lepšie pochopenie architektúry MVC.<br>
-                    Je určený pre študentov predmetu <em>vývoj aplikácií pre internet a intranet</em>, ale nielen im.
-                </p>
+<div class="container">
+    <div class="text-center py-5d">
+        <h1>NoteHub – váš sprievodca organizácie a produktivity!</h1>
+
+        <p>
+            NoteHub je dôveryhodný nástroj v správe úloh a ukladaní myšlienok.
+            Pomáhame vám udržiavať prehľad nad vašimi povinnosťami a nápadmi, aby ste mohli dosiahnuť vaše ciele s ľahkosťou.
+        </p>
+    </div>
+
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-6">
+                <img src="/public/images/calendar.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Váš čas, vaše plány, náš kalendár.</h5>
+                    <p class="card-text">
+                        Náš kalendár je nástrojom, ktorý vám pomôže zlepšiť organizáciu vášho času a mať všetky dôležité dátumy na dosah ruky.
+                        Už nezabudnite na dôležité udalosti a buďte lepšie pripravení na svoj každodenný život.<br>
+                        Ďalšou výhodou nášho kalendára je schopnosť priradiť dôležitosť a prioritu vašim udalostiam, čo vám pomáha identifikovať, ktoré z nich sú najdôležitejšie.
+                        Vďaka farebnej kategorizácii môžete jednoducho rozlíšiť medzi osobnými, pracovnými a rodinnými udalosťami.
+                    </p>
+                    <a href="kalendar.html" class="btn btn-primary">Nastavte svoj čas podľa svojich potrieb ešte dnes!</a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="row mt-3">
-        <div class="col text-center">
-            <h4>Autori</h4>
-            <div>
-                <a href="mailto:Patrik.Hrkut@fri.uniza.sk">doc. Ing. Patrik Hrkút, PhD.</a><br>
-                <a href="mailto:Michal.Duracik@fri.uniza.sk">Ing. Michal Ďuračík, PhD.</a><br>
-                <a href="mailto:Matej.Mesko@fri.uniza.sk">Ing. Matej Meško, PhD.</a><br><br>
-                &copy; 2020-<?= date('Y') ?> Žilinská univerzita v Žiline, Fakulta riadenia a informatiky, Katedra
-                softvérových technológií
+
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-6">
+                <img src="/public/images/notes.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Vytvárajte a spravujte svoje poznámky a myšlienky.</h5>
+                    <p class="card-text">
+                        Vstupte do sekce Poznámky, kde sa vaše myšlienky stávajú realitou.
+                        Toto je miesto, kde môžete vytvárať, upravovať a organizovať svoje poznámky bez obmedzení.
+                        Naše užívateľské rozhranie vám poskytuje jednoduchý a intuitívny spôsob práce s vašimi myšlienkami.
+                        Nemusíte sa ničoho obávať. Vaše myšlienky sú vždy na dosah ruky, takže ich môžete zaznamenávať a sledovať, ako sa menia a rozvíjajú.
+                    </p>
+                    <a href="poznamky.html" class="btn btn-primary">Nechajte kreativitu voľne plynúť a začnite písať!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-6">
+                <img src="/public/images/nastenka.jpg" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Úlohy - Vaša cesta k efektivite</h5>
+                    <p class="card-text">
+                        <span style="color: hotpink;">To-Do (Na vykonanie):</span>
+                        Miesto, kde vaše plány začínajú. Vytvorte si zoznam úloh, ktoré chcete splniť, a sledujte ich, ako sa menia na skutočnosť.<br>
+                        <span style="color: orange;">In Progress (V procese):</span>
+                        Úlohy, na ktorých aktuálne pracujete. Udržujte si prehľad o pokroku a posúvajte sa bližšie k vašim cieľom.<br>
+                        <span style="color: green;">Done (Hotovo):</span>
+                        Tá najlepšia časť. V tejto kategórii zhromažďujete úlohy, ktoré ste úspešne dokončili.
+                        Oslavujte svoje úspechy a majte prehľad o splnených veciach.<br>
+                        S touto prehľadnou organizáciou úloh budete mať stále všetko pod kontrolou.
+                        Bez ohľadu na to, či ide o malé bežné úlohy alebo veľké projekty, jednoduché a efektívne rozdelenie vám pomôže dosiahnuť vaše ciele s ľahkosťou.<br>
+                    </p>
+                    <a href="ulohy.html" class="btn btn-primary">Začnite plánovať, pracovať a oslavovať svoje úspechy už dnes!</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -8,32 +8,48 @@
 <html lang="sk">
 <head>
     <title><?= \App\Config\Configuration::APP_NAME ?></title>
+    <link rel="icon" href="/public/images/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="public/css/styl.css">
-    <script src="public/js/script.js"></script>
+    <link rel="stylesheet" href="/public/css/styl.css">
+    <script src="/public/js/script.js"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-sm bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?= $link->url("home.index") ?>">
-            <img src="public/images/vaiicko_logo.png" title="<?= \App\Config\Configuration::APP_NAME ?>"
+            <img src="/public/images/icon.png" title="<?= \App\Config\Configuration::APP_NAME ?>"
                  title="<?= \App\Config\Configuration::APP_NAME ?>">
         </a>
-        <ul class="navbar-nav me-auto">
+        <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="<?= $link->url("home.contact") ?>">Kontakt</a>
+                <a class="nav-link" href="<?= $link->url("home.index") ?>">Domov</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $link->url("home.contact") ?>">Kalendár</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $link->url("home.contact") ?>">Poznámky</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $link->url("home.contact") ?>">Úlohy</a>
             </li>
         </ul>
         <?php if ($auth->isLogged()) { ?>
-            <span class="navbar-text">Prihlásený používateľ: <b><?= $auth->getLoggedUserName() ?></b></span>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="<?= $link->url("auth.logout") ?>">Odhlásenie</a>
+                    <span class="navbar-text"><b><?= $auth->getLoggedUserName() ?></b></span>
                 </li>
             </ul>
         <?php } else { ?>
@@ -48,6 +64,40 @@
 <div class="container-fluid mt-3">
     <div class="web-content">
         <?= $contentHTML ?>
+    </div>
+</div>
+<div class="text-center">
+    <div class="row">
+        <div class="col">
+            <a class="nav-link" href="<?= $link->url("home.index") ?>">
+                <img src="/public/images/icon.png" alt="Logo" width="40" height="32" class="d-inline-block align-text-middle">
+                NoteHub
+            </a>
+            <a href="<?= $link->url("home.index") ?>">Domov</a><br>
+            <a href="kalendar.html">Kalendár</a><br>
+            <a href="poznamky.html">Poznámky</a><br>
+            <a href="ulohy.html">Úlohy</a>
+        </div>
+
+        <div class="col text-center">
+            <h4>Autori Frameworku:</h4>
+            <div>
+                <a href="mailto:Patrik.Hrkut@fri.uniza.sk">doc. Ing. Patrik Hrkút, PhD.</a><br>
+                <a href="mailto:Michal.Duracik@fri.uniza.sk">Ing. Michal Ďuračík, PhD.</a><br>
+                <a href="mailto:Matej.Mesko@fri.uniza.sk">Ing. Matej Meško, PhD.</a><br><br>
+                &copy; 2020-<?= date('Y') ?> Žilinská univerzita v Žiline, Fakulta riadenia a informatiky, Katedra
+                softvérových technológií
+            </div>
+        </div>
+
+        <div class="col">
+            <h4>Autor stránky:</h4>
+            <a href="mailto: adobrnusak@gmail.com">Andrej Brnušák</a><br>
+        </div>
+
+        <div class="row mt-3">
+
+        </div>
     </div>
 </div>
 </body>
